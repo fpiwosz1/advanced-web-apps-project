e41e8f911209aafbed5429f8443c9b2c76b0e5d6
fpiwosz1 <fpiwosz@cisco.com>
2025-11-17 22:42:35 +0100

    add auth service

---
+++ auth-service/src/test/java/edu/pw/auth/AuthserviceTest.java
@@ -0,0 +1,21 @@
+package edu.pw.auth;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class AuthserviceTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
